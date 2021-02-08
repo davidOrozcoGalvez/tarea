45f06cfd7cf9b9935451f428ca2f94c7ba5f144e
David Orozco <fox.716195@gmail.com>
2021-02-07 20:53:48 -0600

    tarea

---
+++ variables y estructura de control1.js
@@ -0,0 +1,32 @@
+var mes =10;
+var mes=prompt ("Que mes es")
+
+
+if (mes ==12|| mes==1|| mes==2)
+{
+alert("Es Invierno")
+}
+   
+else if (mes >=3 && mes <=5)
+{
+ alert("Es Primavera")
+}
+
+else if (mes >=6 && mes <=8)
+{
+  alert("Es Verano")
+}
+  
+else if (mes >=9 && mes <=10)
+{
+alert("Es Otoño")
+}
+else if(mes >=11 && mes <12)
+{
+  alert("Es Otoño")
+alert("No. 21000783||nombre: David Oswaldo Orozco Galvez")
+  }
+else if (mes >=12)
+{
+  alert("MES INEXISTENTE")
+  }
